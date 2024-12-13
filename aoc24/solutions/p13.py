2f9d0fa32b31d1c57b6e51cb52984d48f314f61a
Carl Egbert <egbertcarl@gmail.com>
2024-12-12 22:47:33 -0800

    day 13 p1 slow version

--- aoc24/solutions/p13.py
+++ aoc24/solutions/p13.py
@@ -1,10 +1,49 @@
+from functools import cache
+import math
 import re
 import sys
+from typing import Iterable, Union
 import unittest
 
 
+Cartesian = tuple[int, int]
+Game = tuple[Cartesian, Cartesian, Cartesian]
+
+
+INT_REX = r"\d+"
+
+
+@cache
+def solve_game(game: Game, coins: int) -> Union[float, int]:
+    a, b, target = game
+    if target == (0, 0):
+        return coins
+
+    x, y = target
+    if x < 0 or y < 0:
+        return math.inf
+
+    ax, ay = a
+    bx, by = b
+
+    return min(
+        solve_game((a, b, (x - ax, y - ay)), coins + 3),
+        solve_game((a, b, (x - bx, y - by)), coins + 1),
+    )
+
+
+def get_games(fname: str) -> Iterable[Game]:
+    with open(fname) as fptr:
+        sections = fptr.read().split("\n\n")
+        for section in sections:
+            ints = [int(x) for x in re.findall(INT_REX, section)]
+            yield (ints[0], ints[1]), (ints[2], ints[3]), (ints[4], ints[5])
+
+
 def solve_p1(fname: str) -> int:
-    return 0
+    coins = [solve_game(game, 0) for game in get_games(fname)]
+
+    return sum([int(c) for c in coins if c != math.inf])
 
 
 def solve_p2(fname: str) -> int:
@@ -13,21 +52,7 @@ def solve_p2(fname: str) -> int:
 
 class TestCase(unittest.TestCase):
     def test_p1(self):
-        self.assertEqual(solve_p1("test_inputs/day_13.txt"), 0)
+        self.assertEqual(solve_p1("aoc24/test_inputs/day_13.txt"), 480)
 
     def test_p2(self):
-        self.assertEqual(solve_p2("test_inputs/day_13.txt"), 0)
-
-
-if __name__ == "__main__":
-    filename = "inputs/day_13.txt"
-    if len(sys.argv) == 1:
-        result = "ERROR: Specify part 1 or 2."
-    elif sys.argv[1] == '1':
-        result = solve_p1(filename)
-    elif sys.argv[1] == '2':
-        result = solve_p2(filename)
-    else:
-        result = "ERROR: Specify part 1 or 2."
-
-    print(result)
+        self.assertEqual(solve_p2("aoc24/test_inputs/day_13.txt"), 0)
