a130c95db1ae03cd097c4bbd00f60296e7f1a904
Carl Egbert <egbertcarl@gmail.com>
2024-12-16 11:38:28 -0800

    day 16

--- aoc24/lib/cartesian.py
+++ aoc24/lib/cartesian.py
@@ -6,6 +6,8 @@ RIGHT = 1, 0
 UP = 0, -1
 DOWN = 0, 1
 
+ALL_DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
+
 
 def cartesian_add(a: Cartesian, b: Cartesian) -> Cartesian:
     x, y = a
@@ -17,3 +19,10 @@ def cartesian_add_n(a: Cartesian, b: Cartesian, n: int) -> Cartesian:
     x, y = a
     bx, by = b
     return x + bx * n, y + by * n
+
+
+def is_opposite_direction(a: Cartesian, b: Cartesian) -> bool:
+    ax, ay = a
+    bx, by = b
+
+    return ax == bx and ay != by or ax != bx and ay == by
--- aoc24/solutions/p16.py
+++ aoc24/solutions/p16.py
@@ -1,10 +1,65 @@
-import re
-import sys
+import heapq
+import math
+from typing import Literal, cast
 import unittest
+
+from aoc24.lib.cartesian import (
+    ALL_DIRECTIONS,
+    RIGHT,
+    Cartesian,
+    cartesian_add,
+    is_opposite_direction,
+)
+from aoc24.lib.grid import Grid, build_grid
+
+
+C = Literal["#", ".", "S", "E"]
+G = Grid[C]
+
+
+def parse_input(fname: str) -> tuple[G, Cartesian, Cartesian]:
+    start = end = (-1, -1)
+    grid = build_grid(fname)
+
+    for k, v in grid.items():
+        if v == "S":
+            start = k
+        if v == "E":
+            end = k
+
+    return grid, start, end
+
+
+def dijkstra(grid: G, start: Cartesian) -> dict[tuple[Cartesian, Cartesian], int]:
+    distances: dict[tuple[Cartesian, Cartesian], int] = {(start, RIGHT): 0}
+    h: list[tuple[int, tuple[Cartesian, Cartesian]]] = [(0, (start, RIGHT))]
+    visited: set[tuple[Cartesian, Cartesian]] = set()
+
+    while h:
+        distance, node = heapq.heappop(h)
+        visited.add(node)
+        location, direction = node
+        moves = [d for d in ALL_DIRECTIONS if not is_opposite_direction(direction, d)]
+        moves = [m for m in moves if grid.get(cartesian_add(location, m), "#") != "#"]
+        for m in moves:
+            new_location = cartesian_add(location, m) if m == direction else location
+            key = (new_location, m)
+            if key in visited:
+                continue
+
+            cost = 1 if m == direction else 1000
+            new_distance = distance + cost
+            if distances.get(key, math.inf) >= new_distance:
+                distances[key] = new_distance
+                heapq.heappush(h, (new_distance, key))
+
+    return distances
 
 
 def solve_p1(fname: str) -> int:
-    return 0
+    grid, start, end = parse_input(fname)
+    distance_mapping = dijkstra(grid, start)
+    return min([distance_mapping[(end, dir)] for dir in ALL_DIRECTIONS])
 
 
 def solve_p2(fname: str) -> int:
@@ -13,21 +68,13 @@ def solve_p2(fname: str) -> int:
 
 class TestCase(unittest.TestCase):
     def test_p1(self):
-        self.assertEqual(solve_p1("test_inputs/day_16.txt"), 0)
+        self.assertEqual(solve_p1("aoc24/test_inputs/day_16.txt"), 7036)
+
+    def test_p1_b(self):
+        self.assertEqual(solve_p1("aoc24/test_inputs/day_16_b.txt"), 11048)
 
     def test_p2(self):
-        self.assertEqual(solve_p2("test_inputs/day_16.txt"), 0)
+        self.assertEqual(solve_p2("aoc24/test_inputs/day_16.txt"), 45)
 
-
-if __name__ == "__main__":
-    filename = "inputs/day_16.txt"
-    if len(sys.argv) == 1:
-        result = "ERROR: Specify part 1 or 2."
-    elif sys.argv[1] == '1':
-        result = solve_p1(filename)
-    elif sys.argv[1] == '2':
-        result = solve_p2(filename)
-    else:
-        result = "ERROR: Specify part 1 or 2."
-
-    print(result)
+    def test_p2_b(self):
+        self.assertEqual(solve_p2("aoc24/test_inputs/day_16_b.txt"), 64)
